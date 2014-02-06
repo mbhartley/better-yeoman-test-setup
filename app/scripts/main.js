a3398ed5490fba3938d78e043ac99416372778e9
Matt Hartley <mbhartley1490@gmail.com>
2014-02-06 14:25:26 -0500

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+function Collection (models) {
+  this.models = models || [];
+ 
+  this.find = function(id) {
+    var result;
+ 
+    if (typeof(id) != 'string') {
+      throw new Error("Whoops!");
+    }
+ 
+    this.models.forEach(function(value, index){
+      if (value.id == id) {
+        result = value;
+      }
+    });
+ 
+    if (result) {
+      return result;
+    }
+  }
+}
