df76f8cc6379b492644c5c6c35e191de4c452d51
Matt Hartley <mbhartley1490@gmail.com>
2014-02-08 10:14:35 -0500

    .add()method, 3 of 4 tests written

--- test/spec/test.js
+++ test/spec/test.js
@@ -4,7 +4,7 @@
     'use strict';
  
     describe('A Collection constructor', function () {
-        describe(', when run', function () {
+        describe('when run', function () {
             it('should return a new object', function () {
               var students = new Collection;
  
@@ -12,7 +12,7 @@
             });
  
             it('stores its first param in a property called models', function(){
-              var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id:'2'}]
+             var toBeAdded = [{name: 'Bower', id: '1'},{name: 'Jack', id:'2'}]
               var students = new Collection(toBeAdded)
               
               expect(students.models).to.be.a('Array');
@@ -24,42 +24,57 @@
  
     describe("A Collection instance", function(){
       describe("has a .find() method",function(){
-        it("should return an object when given an id that is present in the models", function(){
-          var students = new Collection([{name: 'Jim', id: '99'}]);
-          expect(students.find('99')).to.deep.equal({name: 'Jim', id: '99'})
-        });
- 
-        it("should not return an object when that id is not present in the models", function(){
-          var students = new Collection([{name: 'Jim', id: '99'}]);
-          expect(students.find('1')).to.not.equal({name: 'Jim', id: '99'});
-        });
- 
-        it("should return undefined when that id is not present in the models", function(){
-          var students = new Collection([{name: 'Jim', id: '99'}]);
-          expect(students.find('1')).to.equal(undefined);
-        });
- 
-        it("should throw an error when given an arguemnt other than a string", function(){
-          var students = new Collection([{name: 'Jim', id: '99'}]);
-          expect(function(){students.find(1)}).to.throw(Error);
-          expect(function(){students.find({})}).to.throw(Error);
-          expect(function(){students.find([])}).to.throw(Error);
-        });
+        it("should return an object when given an id that is present in the models");
+        it("should not return an object when that id is not present in the models");
+        it("should return undefined when that id is not present in the models");
+        it("should throw an error when given an arguemnt other than a string");
       });
- 
+//my work begins here for commit 1//
       describe("has an .add() method",function(){
-        it("should add the object it's given to the models property");
-        it("should increase the models property's length by 1");
-        it("should only accept a single object as an argument");
-        it("should not  accept an empty object as an argument");
-        it("should throw an error when given an object without and id property");
+        it("should add the object it's given to the models property", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+            students.add({name: 'Billy Jo', id: '8'});
+
+            expect(students.models[1]).to.deep.equal({name: 'Billy Jo', id: '8'});
+        });
+
+        it("should throw an error if an empty object is passed as an argument", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+
+          expect(function(){students.add({})}).to.throw(Error);
+        });
+
+        it("should throw an error when given an object without an id property", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+
+          expect(function(){students.add({name: 'Joey'})}).to.throw(Error);
+        });
+
+        it("should increase the length of the array by 1 when a new object is added", function(){
+          var students = new Collection([{name: 'Jim', id: '99'}]);
+            var startingLength = students.length;
+            students.add({name: 'Sally', id: '23'});
+
+            expect(students.length).to.equal(startingLength + 1);  
+        });    
       });
  
       describe("has a .remove() method",function(){
-        it("should, when given an id, remove the corresponding object from the models property")
+        it("should, when given an id, remove the corresponding object from the models property");
         it("should decrease the models property's length by 1");
         it("should only accept a single string as an id argument");
         it("should return true on successful removal");
+      })
+      describe("has an .empty() method", function(){
+        it('should clear out the models array');
+      })
+ 
+      describe("has an .random() method", function(){
+        it('should return a random object from the models array');
+      });
+ 
+      describe("has a .length() method", function(){
+        it('should return the length models array');
       })
     })
 })();
