fc4b2886ac03dff3096510ed8230c97b599a910d
Matt Hartley <mbhartley1490@gmail.com>
2014-02-08 10:21:18 -0500

    .add()method 3 of 4 tests passing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,19 @@
 function Collection (models) {
  this.models = models || [];
+ 
+  this.add = function(addObject){
+    var students = addObject;
+
+    if (_.isEmpty(addObject)){
+      throw new Error("Nope!");
+    }; 
+
+    if ('id' in students != true){  
+      throw new Error("No id present!");
+    }; 
+
+    this.models.push(addObject);
+  };
+
+
 }
